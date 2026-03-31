22d3749688ce18037d8d2ac72b8a9c5c886ec037
Gb-Mc <sjfh204@gmail.com>
2026-03-30 22:38:49 -0300

    alteração para code smell long method

--- codegen-lite/src/main/java/software/amazon/awssdk/codegen/lite/regions/EndpointTagGenerator.java
+++ codegen-lite/src/main/java/software/amazon/awssdk/codegen/lite/regions/EndpointTagGenerator.java
@@ -1,18 +1,3 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package software.amazon.awssdk.codegen.lite.regions;
 
 import static javax.lang.model.element.Modifier.FINAL;
@@ -51,8 +36,7 @@ public class EndpointTagGenerator implements PoetClass {
     private final Partitions partitions;
     private final String basePackage;
 
-    public EndpointTagGenerator(Partitions partitions,
-                                String basePackage) {
+    public EndpointTagGenerator(Partitions partitions, String basePackage) {
         this.partitions = partitions;
         this.basePackage = basePackage;
     }
@@ -60,143 +44,172 @@ public class EndpointTagGenerator implements PoetClass {
     @Override
     public TypeSpec poetClass() {
         TypeSpec.Builder builder = TypeSpec.classBuilder(className())
-                                           .addModifiers(FINAL, PUBLIC)
-                                           .addJavadoc(documentation())
-                                           .addAnnotation(SdkPublicApi.class)
-                                           .addAnnotation(AnnotationSpec.builder(Generated.class)
-                                                                        .addMember("value",
-                                                                                   "$S",
-                                                                                   "software.amazon.awssdk:codegen")
-                                                                        .build())
-                                           .addMethod(MethodSpec.constructorBuilder()
-                                                                .addModifiers(PRIVATE)
-                                                                .addParameter(String.class, "id")
-                                                                .addStatement("this.id = id")
-                                                                .build());
+                .addModifiers(FINAL, PUBLIC)
+                .addJavadoc(documentation())
+                .addAnnotation(SdkPublicApi.class)
+                .addAnnotation(AnnotationSpec.builder(Generated.class)
+                        .addMember("value", "$S", "software.amazon.awssdk:codegen")
+                        .build())
+                .addMethod(MethodSpec.constructorBuilder()
+                        .addModifiers(PRIVATE)
+                        .addParameter(String.class, "id")
+                        .addStatement("this.id = id")
+                        .build());
+
         endpointTags(builder);
 
         builder.addField(FieldSpec.builder(String.class, "id")
-                                  .addModifiers(FINAL, PRIVATE)
-                                  .build())
-               .addMethod(tagOf())
-               .addMethod(tagGetter())
-               .addMethod(id())
-               .addMethod(tagToString());
+                .addModifiers(FINAL, PRIVATE)
+                .build())
+                .addMethod(tagOf())
+                .addMethod(tagGetter())
+                .addMethod(id())
+                .addMethod(tagToString());
 
         return builder.addType(cache()).build();
     }
 
+    // método alterado
     private void endpointTags(TypeSpec.Builder builder) {
-        Stream<Endpoint> endpointsFromPartitions = partitions.getPartitions().stream().map(Partition::getDefaults);
-        Stream<Endpoint> endpointsFromServices = partitions.getPartitions().stream()
-                                                           .flatMap(p -> p.getServices().values().stream())
-                                                           .flatMap(s -> s.getEndpoints().values().stream());
-        Stream<Endpoint> endpointsFromServiceDefaults = partitions.getPartitions().stream()
-                                                                  .flatMap(p -> p.getServices().values().stream())
-                                                                  .map(Service::getDefaults)
-                                                                  .filter(Objects::nonNull);
+        Set<String> allTags = collectAllTags();
+        addTagFields(builder, allTags);
+        addTagsListField(builder, allTags);
+    }
 
-        Set<String> allTags =
-            Stream.concat(endpointsFromPartitions, Stream.concat(endpointsFromServices, endpointsFromServiceDefaults))
-                  .flatMap(e -> e.getVariants().stream())
-                  .flatMap(v -> v.getTags().stream())
-                  .collect(Collectors.toCollection(TreeSet::new));
+    // método auxiliar novo
+    private Set<String> collectAllTags() {
+        List<Partition> partitionsList = partitions.getPartitions();
 
-        // Add each tag as a separate entry in the endpoint tags
-        allTags.forEach(tag -> builder.addField(FieldSpec.builder(className(), enumValueForTagId(tag))
-                                                         .addModifiers(PUBLIC, STATIC, FINAL)
-                                                         .initializer("$T.of($S)", className(), tag)
-                                                         .build()));
+        Stream<Endpoint> endpointsFromPartitions = partitionsList.stream()
+                .map(Partition::getDefaults)
+                .filter(Objects::nonNull);
 
-        String tagsCodeBlock = allTags.stream()
-                                      .map(this::enumValueForTagId)
-                                      .collect(Collectors.joining(", "));
+        Stream<Endpoint> endpointsFromServices = partitionsList.stream()
+                .flatMap(p -> p.getServices().values().stream())
+                .flatMap(s -> s.getEndpoints().values().stream());
 
-        CodeBlock initializer = CodeBlock.builder()
-                                         .add("$T.unmodifiableList($T.asList(", Collections.class, Arrays.class)
-                                         .add(tagsCodeBlock)
-                                         .add("))")
-                                         .build();
+        Stream<Endpoint> endpointsFromServiceDefaults = partitionsList.stream()
+                .flatMap(p -> p.getServices().values().stream())
+                .map(Service::getDefaults)
+                .filter(Objects::nonNull);
 
-        TypeName listOfTags = ParameterizedTypeName.get(ClassName.get(List.class), className());
+        return Stream.of(
+                endpointsFromPartitions,
+                endpointsFromServices,
+                endpointsFromServiceDefaults)
+                .flatMap(s -> s)
+                .flatMap(this::extractTags)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private Stream<String> extractTags(Endpoint endpoint) {
+        return endpoint.getVariants().stream()
+                .map(v -> v.getTags())
+                .filter(Objects::nonNull)
+                .flatMap(List::stream);
+    }
+
+    // método auxiliar novo
+    private void addTagFields(TypeSpec.Builder builder, Set<String> tags) {
+        tags.forEach(tag -> builder.addField(FieldSpec.builder(className(), enumValueForTagId(tag))
+                .addModifiers(PUBLIC, STATIC, FINAL)
+                .initializer("$T.of($S)", className(), tag)
+                .build()));
+    }
+
+    // método auxiliar novo
+    private void addTagsListField(TypeSpec.Builder builder, Set<String> tags) {
+        CodeBlock.Builder cb = CodeBlock.builder()
+                .add("$T.unmodifiableList($T.asList(", Collections.class, Arrays.class);
+
+        boolean first = true;
+        for (String tag : tags) {
+            if (!first) {
+                cb.add(", ");
+            }
+            cb.add("$L", enumValueForTagId(tag));
+            first = false;
+        }
+
+        cb.add("))");
+
+        TypeName listOfTags = ParameterizedTypeName.get(
+                ClassName.get(List.class),
+                className());
+
         builder.addField(FieldSpec.builder(listOfTags, "ENDPOINT_TAGS")
-                                  .addModifiers(PRIVATE, STATIC, FINAL)
-                                  .initializer(initializer)
-                                  .build());
+                .addModifiers(PRIVATE, STATIC, FINAL)
+                .initializer(cb.build())
+                .build());
     }
 
     private String enumValueForTagId(String tag) {
         return Stream.of(CodegenNamingUtils.splitOnWordBoundaries(tag))
-                     .map(StringUtils::upperCase)
-                     .collect(Collectors.joining("_"));
+                .map(StringUtils::upperCase)
+                .collect(Collectors.joining("_"));
     }
 
     private MethodSpec tagOf() {
         return MethodSpec.methodBuilder("of")
-                         .addModifiers(PUBLIC, STATIC)
-                         .addParameter(String.class, "id")
-                         .returns(className())
-                         .addStatement("return EndpointTagCache.put($L)", "id")
-                         .build();
-
+                .addModifiers(PUBLIC, STATIC)
+                .addParameter(String.class, "id")
+                .returns(className())
+                .addStatement("return EndpointTagCache.put($L)", "id")
+                .build();
     }
 
     private MethodSpec id() {
         return MethodSpec.methodBuilder("id")
-                         .addModifiers(PUBLIC)
-                         .returns(String.class)
-                         .addStatement("return this.id")
-                         .build();
+                .addModifiers(PUBLIC)
+                .returns(String.class)
+                .addStatement("return this.id")
+                .build();
     }
 
     private MethodSpec tagGetter() {
         return MethodSpec.methodBuilder("endpointTags")
-                         .addModifiers(PUBLIC, STATIC)
-                         .returns(ParameterizedTypeName.get(ClassName.get(List.class),
-                                                            className()))
-                         .addStatement("return $L", "ENDPOINT_TAGS")
-                         .build();
+                .addModifiers(PUBLIC, STATIC)
+                .returns(ParameterizedTypeName.get(ClassName.get(List.class), className()))
+                .addStatement("return ENDPOINT_TAGS")
+                .build();
     }
 
     private MethodSpec tagToString() {
         return MethodSpec.methodBuilder("toString")
-                         .addAnnotation(Override.class)
-                         .addModifiers(PUBLIC)
-                         .returns(String.class)
-                         .addStatement("return $L", "id")
-                         .build();
+                .addAnnotation(Override.class)
+                .addModifiers(PUBLIC)
+                .returns(String.class)
+                .addStatement("return id")
+                .build();
     }
 
     private TypeSpec cache() {
         ParameterizedTypeName mapOfStringTags = ParameterizedTypeName.get(ClassName.get(ConcurrentHashMap.class),
-                                                                            ClassName.get(String.class),
-                                                                            className());
+                ClassName.get(String.class),
+                className());
 
         return TypeSpec.classBuilder("EndpointTagCache")
-                       .addModifiers(PRIVATE, STATIC)
-                       .addField(FieldSpec.builder(mapOfStringTags, "IDS")
-                                          .addModifiers(PRIVATE, STATIC, FINAL)
-                                          .initializer("new $T<>()", ConcurrentHashMap.class)
-                                          .build())
-
-                       .addMethod(MethodSpec.constructorBuilder().addModifiers(PRIVATE).build())
-                       .addMethod(MethodSpec.methodBuilder("put")
-                                            .addModifiers(PRIVATE, STATIC)
-                                            .addParameter(String.class, "id")
-                                            .returns(className())
-                                            .addStatement("return $L.computeIfAbsent(id, $T::new)",
-                                                          "IDS",
-                                                          className())
-                                            .build())
-                       .build();
+                .addModifiers(PRIVATE, STATIC)
+                .addField(FieldSpec.builder(mapOfStringTags, "IDS")
+                        .addModifiers(PRIVATE, STATIC, FINAL)
+                        .initializer("new $T<>()", ConcurrentHashMap.class)
+                        .build())
+                .addMethod(MethodSpec.constructorBuilder().addModifiers(PRIVATE).build())
+                .addMethod(MethodSpec.methodBuilder("put")
+                        .addModifiers(PRIVATE, STATIC)
+                        .addParameter(String.class, "id")
+                        .returns(className())
+                        .addStatement("return IDS.computeIfAbsent(id, $T::new)", className())
+                        .build())
+                .build();
     }
 
     private CodeBlock documentation() {
         return CodeBlock.builder()
-                        .add("A tag applied to endpoints to specify that they're to be used in certain contexts. For example, "
-                             + "FIPS tags are applied to endpoints discussed here: https://aws.amazon.com/compliance/fips/ and "
-                             + "DUALSTACK tags are applied to endpoints that can return IPv6 addresses.")
-                        .build();
+                .add("A tag applied to endpoints to specify that they're to be used in certain contexts. For example, "
+                        + "FIPS tags are applied to endpoints discussed here: https://aws.amazon.com/compliance/fips/ and "
+                        + "DUALSTACK tags are applied to endpoints that can return IPv6 addresses.")
+                .build();
     }
 
     @Override
